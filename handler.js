49a2db81f1ccda575741c499e4db38c1180634a2
heshima18 <122611275+heshima18@users.noreply.github.com>
2023-10-27 13:51:13 +0200

    some error

--- handler.js
+++ handler.js
@@ -11,18 +11,23 @@ export const server = app.listen(port,()=>{
 
 const applicationPath = path.join(__dirname,'fingerprint-sdk','fp-browser-service.exe')
 
-const childProcess = spawn(applicationPath, [], {
-    detached: false, // Child process is not detached from the parent
-    stdio: 'inherit' // Inherit standard input, output, and error
-  });
+try {
+  const childProcess = spawn(applicationPath, [], {
+      detached: false, // Child process is not detached from the parent
+      stdio: 'inherit' // Inherit standard input, output, and error
+    });
+    
+    // You can choose to listen for 'exit' events or perform other tasks as needed
+    childProcess.on('exit', (code) => {
+      console.log(`Child process exited with code ${code}`);
+    });
+    
+    // Handle SIGINT (Ctrl+C) to gracefully exit the child process when your Node.js app is closed
+    process.on('SIGINT', () => {
+      childProcess.kill(); // Terminate the child process
+      process.exit(); // Exit the Node.js app
+    });
   
-  // You can choose to listen for 'exit' events or perform other tasks as needed
-  childProcess.on('exit', (code) => {
-    console.log(`Child process exited with code ${code}`);
-  });
-  
-  // Handle SIGINT (Ctrl+C) to gracefully exit the child process when your Node.js app is closed
-  process.on('SIGINT', () => {
-    childProcess.kill(); // Terminate the child process
-    process.exit(); // Exit the Node.js app
-  });
+} catch (error) {
+  console.log(`error starting the fp app \n the error is: \n`, error)
+}
